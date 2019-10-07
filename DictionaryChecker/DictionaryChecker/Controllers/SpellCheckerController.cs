5cc8e66cdc1ed9559dbcbd2945486ea380954650
Daniel Hofer <dh@x-net.at>
2019-10-07 18:52:38 +0200

    added some test code

---
+++ DictionaryChecker/DictionaryChecker/Controllers/SpellCheckerController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+using System.Runtime.CompilerServices;
+using DictionaryChecker.Services;
+
+namespace DictionaryChecker.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SpellCheckerController : ControllerBase
+    {
+        private readonly IDictionaryReader reader;
+        private readonly IDictionaryComparer comparer;
+
+        // IMPORTANT - WITHOUT CONSTRUCTOR --> NULLREFERENCE
+        public SpellCheckerController(IDictionaryReader reader, IDictionaryComparer comparer)
+        {
+            this.comparer = comparer;
+            this.reader = reader;
+        }
+ 
+        
+        //IActionResult --> return statuscode
+        [HttpPost]
+        public async Task<IEnumerable<string>> Spellcheck([FromBody] string textToCheck)
+        {
+            //Read dictionary
+            string dictionaryText = await reader.ReadDictionary();
+
+
+            // Split dictionary and text into words (by line ending \n)
+            var dictionaryWords = dictionaryText.Replace("\r", "").Split("\n");
+            var wordsToCheck = textToCheck.Split(' ');
+            
+            // Iterate over all words in 
+            return comparer.GetUnknownWords(wordsToCheck, dictionaryWords);
+
+            //return textToCheck.Split(' ').Where(words => Contains(word))
+        }
+       // [InternalsVisibleTo("MyTestingProject")]
+       
+    }
+}
---
+++ DictionaryChecker/DictionaryChecker/Services/DictionaryFileReader.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DictionaryChecker.Services
+{
+    internal class DictionaryFileReader: IDictionaryReader
+    {
+        private readonly ILogger<DictionaryFileReader> logger;
+        private readonly IConfiguration config;
+
+        public DictionaryFileReader(ILogger<DictionaryFileReader> logger, IConfiguration config)
+        {
+            this.logger = logger;
+            this.config = config;
+        }
+
+        
+        //ctor tab tab
+      
+
+        /// <summary>
+        /// Reads the dictionary file
+        /// </summary>
+        /// <returns></returns>
+        public async Task<string> ReadDictionary()
+        {
+            var dictionaryText = "";
+            try
+            {
+                // Read the dictionary
+                var dictFile = config["dictionaryFileName"] ;
+                dictionaryText = await System.IO.File.ReadAllTextAsync(dictFile);
+            }
+            catch (FileNotFoundException ex)
+            {
+                logger.LogError(ex, "Dictionary not found");
+                throw;
+            }
+
+            return dictionaryText;
+        }
+    }
+}
